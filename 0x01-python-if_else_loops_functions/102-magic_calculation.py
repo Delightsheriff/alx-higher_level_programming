13df15c7a69721cdcf9853175838a95d8aa97ca7
Delight Sheriff <delightsheriff@gmail.com>
2023-06-07 20:23:43 +0100

    magic_calc

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def magic_calculation(a, b, c):
-    first_part = a ** b
-    second_part = c ** 2
-    magic_number = first_part + second_part
-    return magic_number
+    if (a < b):
+        return c
+
+    if (c > b):
+        return a + b
+
+    return (a * b) - c
