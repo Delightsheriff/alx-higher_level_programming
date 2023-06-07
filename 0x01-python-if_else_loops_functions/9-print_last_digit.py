122a64e46f9413d565f8b28b0bdb9f5fa7fe338b
Delight Sheriff <delightsheriff@gmail.com>
2023-06-07 17:46:17 +0100

    last digit

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def print_last_digit(number):
+    x = []
+    temp = number % 10 if number >= 0 else (number * -1) % 10
+    x.append(temp)
+    for i in x:
+        print(i, end= '')
+    return temp
