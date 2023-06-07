c7f8d9a779e9d28be0fe87775f4529dc89da34f9
Delight Sheriff <delightsheriff@gmail.com>
2023-06-07 17:52:56 +0100

    last digit-correction

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -5,5 +5,5 @@ def print_last_digit(number):
     temp = number % 10 if number >= 0 else (number * -1) % 10
     x.append(temp)
     for i in x:
-        print(i, end= '')
+        print(i, end='')
     return temp
