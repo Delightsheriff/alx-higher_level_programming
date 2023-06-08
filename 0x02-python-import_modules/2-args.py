3c1d8a5f5eadc9f5d50a084efead41177526feb7
Delight Sheriff <delightsheriff@gmail.com>
2023-06-08 20:16:18 +0100

    args-correction-2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,14 +2,15 @@
 import sys
 
 if __name__ == '__main__':
+    counter = 1
     arguments = sys.argv[1:]
     if len(arguments) == 0:
         print('{:d} arguments:'.format(len(arguments)))
     elif len(arguments) == 1:
         print('{:d} argument:'.format(len(arguments)))
+        print('{:d}: {}'.format(counter, arguments[0]))
     else:
         print('{:d} arguments:'.format(len(arguments)))
-        counter = 1
         for arg in arguments:
             print('{:d}: {}'.format(counter, arg))
             counter += 1
