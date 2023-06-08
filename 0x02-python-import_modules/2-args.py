07d825ed4c09b3de5884d39743793c60fa8377b1
Delight Sheriff <delightsheriff@gmail.com>
2023-06-08 20:22:15 +0100

    test

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,13 +4,13 @@ import sys
 if __name__ == '__main__':
     counter = 1
     arguments = sys.argv[1:]
-    if len(arguments) == 0:
-        print('{:d} arguments:'.format(len(arguments)))
-    elif len(arguments) == 1:
-        print('{:d} argument:'.format(len(arguments)))
-        print('{:d}: {}'.format(counter, arguments[0]))
-    else:
+    if len(arguments) > 1:
         print('{:d} arguments:'.format(len(arguments)))
         for arg in arguments:
             print('{:d}: {}'.format(counter, arg))
             counter += 1
+    elif len(arguments) == 1:
+        print('{:d} argument:'.format(len(arguments)))
+        print('{:d}: {}'.format(counter, arguments[0]))
+    elif len(arguments) == 0:
+        print('{:d} arguments:'.format(len(arguments)))
