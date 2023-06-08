8371e95ef04241cf8ea01dba71e9983b9faf8eca
Delight Sheriff <delightsheriff@gmail.com>
2023-06-08 20:11:26 +0100

    args-correction

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 import sys
 
-arguments = sys.argv[1:]
-if len(arguments) == 0:
-    print('{:d} arguments:'.format(len(arguments)))
-elif len(arguments) == 1:
-    print('{:d} argument:'.format(len(arguments)))
-else:
-    print('{:d} arguments:'.format(len(arguments)))
-    counter = 1
-    for arg in arguments:
-        print('{:d}: {}'.format(counter, arg))
-        counter += 1
+if __name__ == '__main__':
+    arguments = sys.argv[1:]
+    if len(arguments) == 0:
+        print('{:d} arguments:'.format(len(arguments)))
+    elif len(arguments) == 1:
+        print('{:d} argument:'.format(len(arguments)))
+    else:
+        print('{:d} arguments:'.format(len(arguments)))
+        counter = 1
+        for arg in arguments:
+            print('{:d}: {}'.format(counter, arg))
+            counter += 1
