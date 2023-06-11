59c8dff180cb1209169124761e8aec4a4df932fb
Delight Sheriff <delightsheriff@gmail.com>
2023-06-11 11:39:02 +0100

    matrixs

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     if matrix:
-        for i in matrix:
-            for j in i:
-                print('{:d}'.format(j), end=" ")
+        for temp in matrix:
+            i = 1
+            x = len(temp)
+            for temps in temp:
+                if i == x:
+                    print('{:d}'.format(temps), end=" ")
+                else:
+                    print('{:d}'.format(temps), end=' ')
+                i += 1
             print()
