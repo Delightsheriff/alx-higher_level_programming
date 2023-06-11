e871fe6634a7d438186bd432a54ebb0e6deffa30
Delight Sheriff <delightsheriff@gmail.com>
2023-06-11 12:48:10 +0100

    delete-2

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -2,8 +2,7 @@
 def delete_at(my_list=[], idx=0):
     if my_list:
         temp = len(my_list)
-        if idx in range(temp):
-            del my_list[idx]
+        if idx < 0 or idx >= temp:
             return my_list
-        elif idx < 0 or idx >= temp:
-            return my_list
+        del my_list[idx]
+        return my_list
