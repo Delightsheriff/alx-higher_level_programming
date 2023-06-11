56281768cb926768fc2ffd4afc97ffbbd6f8f660
Delight Sheriff <delightsheriff@gmail.com>
2023-06-11 12:44:25 +0100

    delete

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def delete_at(my_list=[], idx=0):
+    if my_list:
+        temp = len(my_list)
+        if idx in range(temp):
+            del my_list[idx]
+            return my_list
+        elif idx < 0:
+            return my_list
+        elif idx not in range(temp):
+            return my_list
