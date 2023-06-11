c48e02a4704618318c6a648842edf30fc9c2a625
Delight Sheriff <delightsheriff@gmail.com>
2023-06-11 10:39:09 +0100

    Replace element

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    temp = len(my_list)
+    if idx in range(temp):
+        my_list[idx] = element
+        return my_list
+    elif idx > temp:
+        return my_list
+    elif idx < 0:
+        return my_list
