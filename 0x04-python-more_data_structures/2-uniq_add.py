6ca58d4e4caabb9264e6da1c937a7172d328f536
Delight Sheriff <delightsheriff@gmail.com>
2023-06-14 22:02:34 +0100

    2. Unique addition-1

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    if my_list:
-        temp = set(my_list)
-        count = 0
-        test = list(temp)
-        for i in test:
-            count += i
-        return count
+    temp = set(my_list)
+    count = 0
+    test = list(temp)
+    for i in test:
+        count += i
+    return count
