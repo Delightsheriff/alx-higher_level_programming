50f61e817667926d6f8a0d45929ac8e4d0ccea6e
Delight Sheriff <delightsheriff@gmail.com>
2023-06-14 22:16:45 +0100

    Number of keys-2

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,2 +1,7 @@
+#!/usr/bin/python3
 def number_keys(a_dictionary):
-    return len(a_dictionary.keys())
+    temp = list(a_dictionary)
+    count = 0
+    for i in temp:
+        count += 1
+    return count
