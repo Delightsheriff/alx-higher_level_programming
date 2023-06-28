76e8329f9bbcb4a09ed09e1181610a06c963db84
Delight Sheriff <delightsheriff@gmail.com>
2023-06-28 13:30:23 +0100

    bytecode-1

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -2,9 +2,19 @@
 import dis
 import math
 
+"""
+MagicClass with size validation and position validation
+"""
+
 
 class MagicClass:
+    """
+    A magic class
+    """
     def __init__(self, radius=0):
+        """
+        initialize class with radius
+        """
         self.__radius = 0
 
         if type(radius) is not int and type(radius) is not float:
@@ -13,7 +23,13 @@ class MagicClass:
         self.__radius = radius
 
     def area(self):
+        """
+        calculate the area
+        """
         return self.__radius ** 2 * math.pi
 
     def circumference(self):
+        """
+        calculate the circumference
+        """
         return 2 * math.pi * self.__radius
