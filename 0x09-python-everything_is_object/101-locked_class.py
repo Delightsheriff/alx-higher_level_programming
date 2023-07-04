5d084d9a8924202f5bcb8226197a78c5ac5a172c
Delight Sheriff <delightsheriff@gmail.com>
2023-07-04 23:08:50 +0100

    low memory

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
+"""
+This is a module that containts a class that prevents users from
+dynmaically creating attributes
+"""
+
+
 class LockedClass:
-    __slots__ = 'first_name'
+    __slots__ = ['first_name']
+
+    def __init__(self):
+        """ Init instance """
+        pass
