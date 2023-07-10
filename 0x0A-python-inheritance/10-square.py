52fd7f70c7304707bbfa4891f96d05b84ab64e45
Delight Sheriff <delightsheriff@gmail.com>
2023-07-11 00:24:19 +0100

    10-square-1

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
+"""Defines a Rectangle subclass Square."""
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
+    """Represent a square."""
+
     def __init__(self, size):
+        """Initialize a new square."""
         super().__init__(size, size)
