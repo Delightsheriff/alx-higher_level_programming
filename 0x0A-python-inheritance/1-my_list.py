c93837c5d0e674c9760556980ea0d2a01967db1d
Delight Sheriff <delightsheriff@gmail.com>
2023-07-10 23:29:54 +0100

    1. My list-1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python3
 """
-contains the MyList class
+A module to prints a list in ascending order
 """
 
 
 class MyList(list):
-    """a subclass of list"""
-    def _init_(self):
-        """initializes the object"""
-        super()._init_()
+    """
+    A class to customize the list class
+    """
 
     def print_sorted(self):
-        """prints the sorted list"""
-        print(sorted(self))
+        """
+        Prints a list in ascending order
+
+        Sort a list and then prints on the output
+        """
+
+        if issubclass(MyList, list):
+            print(sorted(self))
