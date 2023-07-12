d6cf132220ea67cef28c607efdd46c0e9695325a
Delight Sheriff <delightsheriff@gmail.com>
2023-07-12 02:45:25 +0100

    8. Class to JSON-1

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -9,4 +9,8 @@ def class_to_json(obj):
     with simple data structure (list, dictionary, string, integer and boolean)
     for JSON serialization of an object
     """
-    return obj.__dict__
+    json_dict = {}
+    for attr_name, attr_value in obj.__dict__.items():
+        if isinstance(attr_value, (list, dict, str, int, bool)):
+            json_dict[attr_name] = attr_value
+    return json_dict
