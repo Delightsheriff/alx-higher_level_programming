ba0d500331e58549e003e92dc3585a3a12ea2681
Delight Sheriff <delightsheriff@gmail.com>
2023-08-17 15:44:08 +0100

    6. States table-1

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,7 +1,7 @@
 -- 6. States table
 -- A script that creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa) on your MySQL server.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states (
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
 		id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
 		name varchar(256) NOT NULL
 		);
