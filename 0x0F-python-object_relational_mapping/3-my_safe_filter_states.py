7459ad42e0d9350c60f80bd177183572d40a0973
Delight Sheriff <delightsheriff@gmail.com>
2023-09-19 15:06:14 +0100

    3. SQL Injection...

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""script that takes in an argument and displays all values in the states"""
+
+
+import MySQLdb
+from sys import argv
+
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(
+        host="localhost", user=argv[1], port=3306, passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE \
+        BINARY %(name)s ORDER BY states.id ASC", {'name': argv[4]})
+
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
