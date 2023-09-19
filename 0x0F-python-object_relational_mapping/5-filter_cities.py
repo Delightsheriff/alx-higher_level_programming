34818c91b53305dad6099c48d6c73e85a6846c6f
Delight Sheriff <delightsheriff@gmail.com>
2023-09-19 15:32:21 +0100

    5. All cities by state-2

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -27,8 +27,8 @@ if __name__ == '__main__':
     cur.execute(temp, (sys.argv[4],))
 
     rows = cur.fetchall()
-    for row in rows:
-        print(row)
+    if rows is not None:
+        print(", ".join([row[1] for row in rows]))
 
     cur.close()
     db.close()
