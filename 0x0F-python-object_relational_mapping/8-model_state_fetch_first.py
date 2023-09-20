22759f188fe6800332902b01378324a57847b76c
Delight Sheriff <delightsheriff@gmail.com>
2023-09-20 21:04:49 +0100

    8. First state-1

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -2,13 +2,18 @@
 """
 Lists all State objects from the database hbtn_0e_6_usa
 """
-from  sys import argv
+from sys import argv
 from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 
 if __name__ == '__main__':
+    """
+    Access to the database and get a state
+    from the database.
+    """
+
     temp = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
         argv[1], argv[2], argv[3])
 
