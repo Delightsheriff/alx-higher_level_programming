4cb64ff08cab56f496709cb7cace1d29187d247a
Delight Sheriff <delightsheriff@gmail.com>
2023-10-02 23:44:28 +0100

    2. POST an email #0

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -5,20 +5,14 @@ as a parameter, and displays the body of the response
 (decoded in utf-8).
 """
 
-import urllib.request
-import urllib.parse
+from urllib.request import Request, urlopen
+from urllib.parse import urlencode
 import sys
 
 
-url = sys.argv[1]
-email = sys.argv[2]
-data = {"email": email}
-data = urllib.parse.urlencode(data).encode("utf-8")
-request = urllib.request.Request(url, data)
-
-
 if __name__ == "__main__":
-    with urllib.request.urlopen(request) as response:
-        data = response.read()
-        html = data.decode("utf-8")
-        print(html)
+    data = urlencode({"email": sys.argv[2]}).encode('ascii')
+    request = Request(sys.argv[1], data)
+
+    with urlopen(request) as response:
+        print(response.read().decode("utf-8"))
